d749c70ba9877fa2d7f9f91c4d11d48cc0a66bd7
taiseen <taiseen.cse@gmail.com>
2021-05-27 23:23:10 +0600

    null info add

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -7,16 +7,21 @@ function BookContainer() {
 
     const { books, deleteBook } = useContext(BookContext);
 
-    return (
+    return books.length ? (
         <section className="book_section">
-            
+
             {
                 books.map(book => <Book book={book} deleteBook={deleteBook} key={book.id} />)
             }
 
             <BookInput />
-            
+
         </section>
+    ) : (
+        <>
+            <p className="empty">No book in your reading list</p>
+            <BookInput />
+        </>
     );
 }
 
